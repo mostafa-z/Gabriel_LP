b870bbb61307e2b0c2f025528a63b12de78d4897
Dorimanx <yuri@bynet.co.il>
2014-10-14 03:01:29 +0300

    New Touch Boost driver by @Alucard24 and New CPUFREQ_LIMIT V4.0

--- arch/arm/mach-msm/include/mach/cpufreq.h
+++ arch/arm/mach-msm/include/mach/cpufreq.h
@@ -16,6 +16,11 @@
 #if defined(CONFIG_DEVFREQ_GOV_MSM_CPUFREQ)
 extern int devfreq_msm_cpufreq_update_bw(void);
 extern int register_devfreq_msm_cpufreq(void);
+extern unsigned int get_cpu_min_lock(unsigned int cpu);
+extern void set_cpu_min_lock(unsigned int cpu, int freq);
+extern unsigned int get_max_lock(unsigned int cpu);
+extern void set_max_lock(unsigned int cpu, unsigned int freq);
+
 #else
 static int devfreq_msm_cpufreq_update_bw(void)
 {
@@ -25,6 +30,23 @@ static int register_devfreq_msm_cpufreq(void)
 {
 	return 0;
 }
+static unsigned int get_cpu_min_lock(unsigned int cpu)
+{
+	return -ENOSYS;
+}
+static void set_cpu_min_lock(unsigned int cpu, int freq)
+{
+	return -ENOSYS;
+}
+static unsigned int get_max_lock(unsigned int cpu)
+{
+	return -ENOSYS;
+}
+static void set_max_lock(unsigned int cpu, unsigned int freq)
+{
+	return -ENOSYS;
+}
+
 #endif
 
 #if defined(CONFIG_CPU_FREQ_MSM)
