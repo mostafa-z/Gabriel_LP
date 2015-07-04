462652bc84f2de154d3d3009ae8a445790775349
mostafa-z <mostafazarghami@gmail.com>
2015-07-04 20:07:15 +0430

    Fastcharge_initial adaption for G3 first release grabed from hollyangel git and updated https://github.com/holyangel/LGE_G3/commit/88dcd20158e602ecea2b90b146bc2567e0f15455 https://github.com/holyangel/LGE_G3/commit/9bc5c9dff937facd80848168a1798ef46615baa0
     Changes to be committed:
    	modified:   arch/arm/mach-msm/Kconfig
    	modified:   arch/arm/mach-msm/Makefile
    	new file:   arch/arm/mach-msm/fastchg.c
    	modified:   drivers/power/pm8921-charger.c
    	modified:   drivers/usb/otg/msm_otg.c
    	new file:   include/linux/fastchg.h

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -34,6 +34,10 @@
 #include <mach/msm_xo.h>
 #include <mach/msm_hsusb.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define CHG_BUCK_CLOCK_CTRL	0x14
 #define CHG_BUCK_CLOCK_CTRL_8038	0xD
 
@@ -1993,7 +1997,42 @@ static void __pm8921_charger_vbus_draw(unsigned int mA)
 			i--;
 		if (i < 0)
 			i = 0;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if (force_fast_charge == 1)
+			i = 14;
+		else if (force_fast_charge == 2) {
+			switch (fast_charge_level) {
+			case FAST_CHARGE_500:
+				i = 2;
+				custom_ma = FAST_CHARGE_500;
+				break;
+			case FAST_CHARGE_900:
+				i = 3;
+				custom_ma = FAST_CHARGE_900;
+				break;
+			case FAST_CHARGE_1200:
+				i = 4;
+				custom_ma = FAST_CHARGE_1200;
+				break;
+			case FAST_CHARGE_1500:
+				i = 5;
+				custom_ma = FAST_CHARGE_1500;
+				break;
+			case FAST_CHARGE_2000:
+				i = 6;
+				custom_ma = FAST_CHARGE_1800;
+				break;
+			default:
+				break;
+			}
+		}
 		rc = pm_chg_iusbmax_set(the_chip, i);
+		pr_info("charge curent index => %d\n", i);
+#else
+
+		rc = pm_chg_iusbmax_set(the_chip, i);
+#endif
 		if (rc)
 			pr_err("unable to set iusb to %d rc = %d\n", i, rc);
 	}
