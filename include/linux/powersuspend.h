7abdfb2ea845de0b4afe63d6a40fc10020452233
mostafa-z <mostafazarghami@gmail.com>
2015-07-08 02:54:27 +0430

    Powersuspend fix for Adreno idler i mean fix my mistakes and laziness =p  Changes to be committed: 	modified:   include/linux/powersuspend.h 	modified:   kernel/power/powersuspend.c

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -36,12 +36,11 @@
 #define POWER_SUSPEND_AUTOSLEEP	0	// Use kernel autosleep as hook
 #define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
 #define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
-#define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep as hook
 
 struct power_suspend {
-	struct list_head link;
-	void (*suspend)(struct power_suspend *h);
-	void (*resume)(struct power_suspend *h);
+        struct list_head link;
+        void (*suspend)(struct power_suspend *h);
+        void (*resume)(struct power_suspend *h);
 };
 
 void register_power_suspend(struct power_suspend *handler);
@@ -53,5 +52,3 @@ void set_power_suspend_state_panel_hook(int new_state);
 extern bool power_suspended;
 
 #endif
-
-
