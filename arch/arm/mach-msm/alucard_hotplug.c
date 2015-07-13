9ee6f4ce3804c5931052218c3f5d5f07680da2c5
mostafa-z <mostafazarghami@gmail.com>
2015-07-13 23:20:46 +0430

    Alucard_Hotplug Fix in INIT_DEFERRABLE_WORK

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -124,7 +124,7 @@ static int init_rq_avg(void)
 	}
 	spin_lock_init(&rq_data->lock);
 	rq_data->update_rate = RQ_AVG_TIMER_RATE;
-	INIT_DELAYED_WORK_DEFERRABLE(&rq_data->work, rq_work_fn);
+	INIT_DEFERRABLE_WORK(&rq_data->work, rq_work_fn);
 
 	return 0;
 }
@@ -453,7 +453,7 @@ static int hotplug_start(void)
 
 	start_rq_work();
 
-	INIT_DELAYED_WORK_DEFERRABLE(&alucard_hotplug_work, hotplug_work_fn);
+	INIT_DEFERRABLE_WORK(&alucard_hotplug_work, hotplug_work_fn);
 	queue_delayed_work_on(BOOT_CPU, system_wq,
 				&alucard_hotplug_work,
 				msecs_to_jiffies(
