791ba2ff9196bda14d43f836d3389cb57f85ad6e
mostafa-z <mostafazarghami@gmail.com>
2015-07-13 17:16:24 +0430

    New Touch Boost driver by @Alucard24 and New CPUFREQ_LIMIT V4.0
    *Replaced old cpu-boost driver with new fast and great from Alucard24.
    New driver is using new freq limits functions in main cpufreq driver.
    working super fast, locking on all cores, and can be tuned to boost
    on 1->2->3->all, no useless shit that exist in cpu-boost.
    
    *Alucard helped me to create new LG needed function for ROM thermal
    binary. after thermal throttle is over, and core is cold,
    ROM binary cant restore max freq on all cores, so they stay no maxed
    and before the thermal event.
    now it's used to detect max freq set on core 0 when it's limited less.
    then all cores set to there limit max set before.
    ROM binary restore max freq only to core0 or active cores.
    Kernel will help and restore all as should be.
    
    *Cleaned MSM and Intelli Hotplugs from net needed var.
    *Merged from alucard all that needed for new cpu booster code.
    
    Big thanks to @Alucard24 for suprime help and close work on this project!
    
    LP UPDATE NOTE!
    
    This is merge from my KK kernel.
    
    driver at drivers/input/touchscreen/alu-t-boost.c was added on big wave...
    ==================================================================================
    sorry, couldn't cherry-pick ...
    original commit is here, Thanks Dorimanx
    https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/11e16e7204846f82a7468b8c18c256600ea195f9
    
     Changes to be committed:
    	modified:   arch/arm/mach-msm/cpufreq.c
    	modified:   arch/arm/mach-msm/include/mach/cpufreq.h
    	modified:   drivers/cpufreq/Makefile
    	modified:   drivers/cpufreq/cpufreq.c
    	modified:   drivers/input/touchscreen/alu-t-boost.c

--- drivers/input/touchscreen/alu-t-boost.c
+++ drivers/input/touchscreen/alu-t-boost.c
@@ -80,7 +80,7 @@ static void do_input_boost(struct work_struct *work)
 
 	if (nr_cpus <= 0)
 		nr_cpus = 1;
-	if (nr_cpus > NR_CPUS)
+	else if (nr_cpus > NR_CPUS)
 		nr_cpus = NR_CPUS;
 
 	for (i = 0; i < nr_cpus; i++) {
@@ -90,18 +90,17 @@ static void do_input_boost(struct work_struct *work)
 		dprintk("Input boost for CPU%u\n", i);
 		set_cpu_min_lock(i, input_boost_freq);
 
-		cur = cpufreq_quick_get(i);
-		if (cur > 0 && cpu_online(i)) {
-			policy.cpu = i;
-
-			if (cur < input_boost_freq)
+		if (cpu_online(i)) {
+			cur = cpufreq_quick_get(i);
+			if (cur < input_boost_freq && cur > 0) {
+				policy.cpu = i;
 				cpufreq_driver_target(&policy,
-						input_boost_freq,
-						CPUFREQ_RELATION_L);
+					input_boost_freq, CPUFREQ_RELATION_L);
+			}
 		}
 	}
 
-	mod_delayed_work_on(0, touch_boost_wq,
+	mod_delayed_work_on(BOOT_CPU, touch_boost_wq,
 			&input_boost_rem,
 			msecs_to_jiffies(input_boost_ms));
 }
@@ -139,7 +138,7 @@ static int touchboost_input_connect(struct input_handler *handler,
 
 	handle->dev = dev;
 	handle->handler = handler;
-	handle->name = "cpufreq";
+	handle->name = handler->name;
 
 	error = input_register_handle(handle);
 	if (error)
@@ -211,3 +210,4 @@ static int touch_boost_init(void)
 	return ret;
 }
 late_initcall(touch_boost_init);
+
