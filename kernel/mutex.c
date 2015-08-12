ec6d2cd1bf4a23e928c29c7aa487922cc337ca5e
mostafa-z <mostafazarghami@gmail.com>
2015-08-12 23:32:32 +0430

    Comment on spin_owner

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -209,8 +209,9 @@ int mutex_can_spin_on_owner(struct mutex *lock)
 {
 	int retval = 1;
 
-	if (!sched_feat(OWNER_SPIN))
-		return 0;
+// clean-up for now
+/*	if (!sched_feat(OWNER_SPIN))
+		return 0; */
 
 	rcu_read_lock();
 	if (lock->owner)
