29fc40edc84a0eb7e881c2b954db4dfa0ed874f5
Dorimanx <yuri@bynet.co.il>
2015-09-21 01:28:58 +0300

    State Notifier: allow to see suspend ON/OFF in userspace for cortex.
    Also added bool hook for sweep2sleep driver. it's has to suspend
    right away. so touch screen will not allow to end call even if screen
    is black. only Knock ON/CODE is registred.

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -25,6 +25,7 @@
  
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
+bool scr_suspended;
 #endif
 
 #include <linux/lcd_notify.h>
@@ -1272,6 +1273,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 #ifdef CONFIG_STATE_NOTIFIER
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+		scr_suspended = false;
+#endif
 		if (!use_fb_notifier)
 			state_resume();
 #endif
@@ -1289,6 +1293,9 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		rc = mdss_dsi_off(pdata);
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);
 #ifdef CONFIG_STATE_NOTIFIER
+#ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
+		scr_suspended = true;
+#endif
 		if (!use_fb_notifier)
 			state_suspend();
 #endif
